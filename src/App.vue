3ba7f31576b7743f6245a38f23ba7a2e7831d084
Hdden01 <907835825@qq.com>
2021-01-21 14:04:20 +0800

    修改app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>sjasjasjansasn</h2>
   </div>
 </template>
 
@@ -9,10 +8,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
